09094680ae944915cdc3406be1b3b6051f43af8b
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-20 15:52:20 +0530

    day 3

--- input.c
+++ input.c
@@ -13,8 +13,10 @@ int main(void)
 {
     char f_name[100];
     char l_name[100];
+    
     printf("What's your first name: ");
     scanf("%s", f_name);
+
     printf("What's your last name: ");
     scanf("%s", l_name);
 
