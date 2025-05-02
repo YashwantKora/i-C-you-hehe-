bedbccc1e29ce3cc09ef0d0ba39e3ec4b8bd689f
YashwantKora <Yashwantqwerty@gmail.com>
2025-05-02 19:29:56 +0530

    Day4

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("New line");
+    return 0;//serves as an exit code
+}
+
+
+/*preprocessor directive
+main function
+print
+return
+comments
+*/
